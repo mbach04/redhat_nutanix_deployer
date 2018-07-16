b28eff889a28165c8cf80a8aa71a5f755067e5e5
Timothy Ling <tling@redhat.com>
2018-07-16 10:09:01 -0600

    integrate tower role into system

--- roles/tower_prelim/files/ansible_tower_custom.sh
+++ roles/tower_prelim/files/ansible_tower_custom.sh
@@ -1,1 +1,2 @@
-ANSIBLE_BECOME_METHOD=sudo ANSIBLE_BECOME=true ansible-playbook -i inventory_custom -e "bundle_install=true upgrade_ansible_with_tower=true" install.yml
+#!/bin/bash
+ansible-playbook -i inventory_custom -e "bundle_install=true upgrade_ansible_with_tower=true" install.yml
